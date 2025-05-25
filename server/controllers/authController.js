02e2b345a682cebeb14b92a821501d97846e88b0
vithurshan <164380624+VithurshanV2@users.noreply.github.com>
2025-05-25 18:43:25 +0530

    Implement user login logic
    - validate required fields: email and password
    - issue JWT upon successful login and set it as httpOnly cookie

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -32,7 +32,62 @@ export const register = async (req, res) => {
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         });
 
-        return res.status(201).json({ success: true, message: 'User registered', user: { id: user.id, name: user.name, email: user.email } });
+        return res.status(201).json({
+            success: true, message: 'User registered',
+            user: { id: user.id, name: user.name, email: user.email }
+        });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+}
+
+// Login user
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email, !password) {
+        return res.status(400).json({
+            success: false,
+            message: 'Email and password fields required'
+        });
+    }
+
+    try {
+        const user = await getUserByEmail(email);
+
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: 'Invalid email'
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({
+                success: false,
+                message: 'Invalid password'
+            });
+        }
+
+        // Generate JWT token with user's ID
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+        // Set the JWT token as an HTTP-only cookie
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+        });
+
+        return res.status(200).json({
+            success: true, message: 'Login successful',
+            user: { id: user.id, name: user.name, email: user.email }
+        });
 
     } catch (error) {
         console.error(error);
