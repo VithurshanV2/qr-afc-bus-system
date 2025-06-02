6dd41286791162e0ff79c780d59537ce1bff8eec
vithurshan <164380624+VithurshanV2@users.noreply.github.com>
2025-06-02 19:08:34 +0530

    Add Header component and integrate into Home page
    - create Header.jsx with Register button
    - update Home.jsx to include Header

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import Header from '../components/Header';
 
 const Home = () => {
     return (
-        <div>
+        <div className='flex flex-col items-center justify-center min-h-screen '>
             <Navbar />
+            <Header />
         </div>
     );
 };
