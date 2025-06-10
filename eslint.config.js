5502ae64f2465e0f47c2b97f37b286143a203162
vithurshan <164380624+VithurshanV2@users.noreply.github.com>
2025-06-10 11:52:46 +0530

    Refactor ESLint config
    - move all client and server lint rules to root eslint.config.js
    - add react-refresh and custom rules for client scope
    - delete client/eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,15 @@
 import js from '@eslint/js';
 import reactPlugin from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 import prettier from 'eslint-config-prettier';
 
 export default [
+  // Client config
   {
-    files: ['**/*.js', '**/*.jsx'],
+    files: ['client/**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
+      parser: js.parser,
       sourceType: 'module',
       ecmaVersion: 'latest',
       globals: {
@@ -14,16 +17,53 @@ export default [
         document: true,
         navigator: true,
       },
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
     },
     plugins: {
       react: reactPlugin,
       'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactPlugin.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
     },
+  },
+  // Server config
+  {
+    files: ['server/**/*.{js,ts}', '*.js'],
+    languageOptions: {
+      parser: js.parser,
+      sourceType: 'module',
+      ecmaVersion: 'latest',
+      globals: {
+        process: true,
+        __dirname: true,
+        module: true,
+        require: true,
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+    },
+  },
+  // Prettier config
+  {
+    name: 'Prettier',
+    rules: {
+      ...prettier.rules,
+    },
+  },
+  // Global ignores
+  {
     ignores: [
       'node_modules/',
       'build/',
@@ -34,11 +74,5 @@ export default [
       'package-lock.json',
       'yarn.lock',
     ],
-  },
-  {
-    name: 'Prettier',
-    rules: {
-      ...prettier.rules,
-    },
   },
 ];
