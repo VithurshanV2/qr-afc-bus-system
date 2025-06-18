73d10cbde819e4e3c413bb233da327ffc72aa26a
vithurshan <164380624+VithurshanV2@users.noreply.github.com>
2025-06-18 10:52:51 +0530

    Add OTP UI, logic, and theme to verification page
    - implement 6-digit OTP input with auto-focus and paste support
    - add basic styling and layout for verification form
    - define the use @theme colors in index.css

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -1,9 +1,77 @@
 import React from 'react';
+import { assets } from '../assets/assets';
+import { useNavigate } from 'react-router-dom';
 
 const EmailVerify = () => {
+  const navigate = useNavigate();
+
+  // Store refs for OTP input fields
+  const inputRefs = React.useRef([]);
+
+  // Auto-focus next input on entry
+  const handleInput = (e, index) => {
+    if (e.target.value.length > 0 && index < inputRefs.current.length - 1) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
+
+  // Auto-focus previous input on backspace
+  const handleKeyDown = (e, index) => {
+    if (e.key === 'Backspace' && e.target.value === '' && index > 0) {
+      inputRefs.current[index - 1].focus();
+    }
+  };
+
+  // Distribute pasted OTP digits across input fields
+  const handlePaste = (e) => {
+    const paste = e.clipboardData.getData('text');
+    const pasteArray = paste.split('');
+    pasteArray.forEach((char, index) => {
+      if (inputRefs.current[index]) {
+        inputRefs.current[index].value = char;
+      }
+    });
+  };
+
   return (
-    <div>
-      <h1>Account Verification Page</h1>
+    <div
+      className="flex items-center justify-center min-h-screen
+        bg-gradient-to-br from-yellow-200 to-orange-400"
+    >
+      <img
+        onClick={() => navigate('/')}
+        src={assets.logo}
+        alt="logo"
+        className="absolute left-5 sm:left-20 top-5 w-32 sm:w-48 cursor-pointer"
+      />
+
+      <form className="bg-dark-bg p-8 rounded-lg shadow-lg w-96 text-sm">
+        <h1 className="text-white text-2xl font-semibold text-center mb-4">
+          Email Verify OTP
+        </h1>
+        <p className="text-yellow-200 text-center mb-6">
+          Enter the 6-digit code sent to your Email ID
+        </p>
+        <div className="flex justify-between mb-8" onPaste={handlePaste}>
+          {Array(6)
+            .fill(0)
+            .map((_, index) => (
+              <input
+                type="text"
+                maxLength="1"
+                key={index}
+                required
+                className="w-12 h-12 bg-input-bg text-white text-center text-xl rounded-md"
+                ref={(e) => (inputRefs.current[index] = e)}
+                onInput={(e) => handleInput(e, index)}
+                onKeyDown={(e) => handleKeyDown(e, index)}
+              />
+            ))}
+        </div>
+        <button className="w-full py-3 text-white rounded-full bg-gradient-to-r from-yellow-400 to-orange-500">
+          Verify email
+        </button>
+      </form>
     </div>
   );
 };
