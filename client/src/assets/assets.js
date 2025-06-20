848b56774f3b620b035458136b74103e5bc22e04
vithurshan <164380624+VithurshanV2@users.noreply.github.com>
2025-06-20 09:36:58 +0530

    Add submark logo and set as favicon
    - add submark_logo.svg to assets folder
    - update index.html to use submark as favicon
    - import submark_logo in assets.js

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -3,6 +3,7 @@ import lock_icon from './lock_icon.svg';
 import user_icon from './user_icon.svg';
 import mail_icon from './mail_icon.svg';
 import right_arrow_icon from './right_arrow_icon.svg';
+import submark_logo from './submark_logo.svg';
 
 export const assets = {
   logo,
@@ -10,4 +11,5 @@ export const assets = {
   user_icon,
   mail_icon,
   right_arrow_icon,
+  submark_logo,
 };
