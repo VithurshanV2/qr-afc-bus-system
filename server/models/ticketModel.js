f4ca959058a41f8c5870435844fcacf3e4d9c802
vithurshan <164380624+VithurshanV2@users.noreply.github.com>
2025-10-31 19:26:12 +0530

    Enforce ticket expiry and active validation
    - add `expireOldTickets` to mark expired pending tickets
    - add `getActiveTicket` to check existing active tickets
    - update `createTicketAtBoarding` to prevent duplicates
    - include `expiresAt` timestamp (15 minutes) for new tickets

--- server/models/ticketModel.js
+++ server/models/ticketModel.js
@@ -2,17 +2,31 @@ import { PrismaClient } from '../generated/prisma/index.js';
 
 const prisma = new PrismaClient();
 
-// Create ticket at boarding
+// Create ticket at boarding only if no active ticket
 export const createTicketAtBoarding = async ({
   userId,
   tripId,
   boardingHalt,
 }) => {
+  await expireOldTickets(userId);
+
+  const active = await getActiveTicket(userId);
+
+  if (active) {
+    throw new Error(
+      'An active ticket exists. Please either cancel or complete it to proceed',
+    );
+  }
+
+  const expiry = 15 * 60 * 1000; // 15 minutes
+  const expiresAt = new Date(Date.now() + expiry);
+
   return await prisma.ticket.create({
     data: {
       commuterId: userId,
       tripId,
       boardingHalt,
+      expiresAt,
       status: 'PENDING',
     },
   });
@@ -44,5 +58,29 @@ export const setPassengerCount = async (ticketId, adultCount, childCount) => {
   return await prisma.ticket.update({
     where: { id: ticketId },
     data: { adultCount, childCount, status: 'PENDING' },
+  });
+};
+
+// set ticket status to EXPIRED
+export const expireOldTickets = async (userId) => {
+  return await prisma.ticket.updateMany({
+    where: {
+      commuterId: userId,
+      status: 'PENDING',
+      expiresAt: { lt: new Date() },
+    },
+    data: { status: 'EXPIRED' },
+  });
+};
+
+// Get active tickets
+export const getActiveTicket = async (userId) => {
+  return await prisma.ticket.findFirst({
+    where: {
+      commuterId: userId,
+      status: 'PENDING',
+      expiresAt: { gt: new Date() },
+    },
+    orderBy: { id: 'desc' },
   });
 };
